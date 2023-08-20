f9f0f738a26ff1fb3803d0029ff46461566e1619
Abdul Rehman <arehmannasir@outlook.com>
2023-08-20 20:44:47 +0100

    modified the label generator

--- dataset_gen_yolo_label.py
+++ dataset_gen_yolo_label.py
@@ -12,18 +12,26 @@ from label_tools.yolov5.yolov5_helper import *
 
 
 ROOT_PATH = Path(__file__).parent.as_posix()
-RAW_DATA_PATH = "{}/{}".format(ROOT_PATH, 'raw_data')
+RAW_DATA_PATH = "{}/{}".format(ROOT_PATH, 'recording')
 DATASET_PATH = "{}/{}".format(ROOT_PATH, 'dataset')
 
-def gather_yolo_data(record_name: str, vehicle_name: str, rgb_camera_name: str, semantic_camera_name: str):
+def gather_yolo_data(record_name: str, rgb_camera_name: str = 'rgb', semantic_camera_name: str = 'instance'):
     yolo_rawdata_df = pd.DataFrame()
-    vehicle_rawdata_path = f"{RAW_DATA_PATH}/{record_name}/{vehicle_name}"
-    rgb_image_path_list = sorted(glob.glob(f"{vehicle_rawdata_path}/{rgb_camera_name}/*.png"))
-    semantic_image_path_list = sorted(glob.glob(f"{vehicle_rawdata_path}/{semantic_camera_name}/*.png"))
+    record_rawdata_path = f"{RAW_DATA_PATH}/{record_name}"
+    rgb_image_path_list = sorted(glob.glob(f"{record_rawdata_path}/{rgb_camera_name}/*.png"))
+    semantic_image_path_list = sorted(glob.glob(f"{record_rawdata_path}/{semantic_camera_name}/*.png"))
+
+    # Filter only those frames for which both RGB and Semantic images exist
+    rgb_basenames = {os.path.basename(p) for p in rgb_image_path_list}
+    semantic_basenames = {os.path.basename(p) for p in semantic_image_path_list}
+
+    common_basenames = rgb_basenames.intersection(semantic_basenames)
+    rgb_image_path_list = [p for p in rgb_image_path_list if os.path.basename(p) in common_basenames]
+    semantic_image_path_list = [p for p in semantic_image_path_list if os.path.basename(p) in common_basenames]
+
     yolo_rawdata_df['rgb_image_path'] = rgb_image_path_list
     yolo_rawdata_df['semantic_image_path'] = semantic_image_path_list
     yolo_rawdata_df['record_name'] = record_name
-    yolo_rawdata_df['vehicle_name'] = vehicle_name
 
     # Adding train/valid/test splits
     np.random.seed(42)
@@ -31,6 +39,7 @@ def gather_yolo_data(record_name: str, vehicle_name: str, rgb_camera_name: str, 
     yolo_rawdata_df['split'] = yolo_rawdata_df['random_number'].apply(lambda x: 'train' if x < 0.7 else 'val' if x < 0.9 else 'test')
     yolo_rawdata_df = yolo_rawdata_df.drop('random_number', axis=1)
     return yolo_rawdata_df
+
 
 
 class YoloLabelTool:
@@ -46,7 +55,7 @@ class YoloLabelTool:
             frame_names = split_df['rgb_image_path'].apply(get_filename_from_fullpath)
             with open(os.path.join(output_dir, f'{split_name}_yolo.txt'), 'a') as f:
                 for i, frame_name in enumerate(frame_names):
-                    filename = split_df["vehicle_name"].iloc[i] + frame_name
+                    filename = frame_name
                     f.write(f'./images/{split_name}/{filename}.png\n')
 
         start = time.time()
@@ -127,145 +136,23 @@ class YoloLabelTool:
                 labels_all.append(label_info)
 
                 # Debug: draw the bounding box
-                # cv2.rectangle(image_rgb, (x_min, y_min), (x_max, y_max), (0, 255, 0), 1)
+                cv2.rectangle(image_rgb, (x_min, y_min), (x_max, y_max), (0, 255, 0), 1)
 
         if len(labels_all) > 0:
-            write_image(output_dir, frame_id, image_rgb, frame["vehicle_name"], frame["split"])
-            write_label(output_dir, frame_id, labels_all, frame["vehicle_name"], frame["split"])
+            write_image(output_dir, frame_id, image_rgb, frame["split"])
+            write_label(output_dir, frame_id, labels_all, frame["split"])
+
             
         write_yaml(output_dir)
         return
 
-    def process_frame(self, index, frame):
-        rgb_img_path = frame['rgb_image_path']
-        seg_img_path = frame['semantic_image_path']
-        success = check_id(rgb_img_path, seg_img_path)
-        if not success:
-            return
-
-        # output_dir = f"{DATASET_PATH}/{frame['record_name']}/{frame['vehicle_name']}/yolo"
-        output_dir = f"{DATASET_PATH}"
-        frame_id = get_filename_from_fullpath(rgb_img_path)
-
-        image_rgb = None
-        image_seg = None
-        image_rgb = cv2.imread(rgb_img_path, cv2.IMREAD_COLOR)
-        image_seg = cv2.imread(seg_img_path, cv2.IMREAD_UNCHANGED)
-        if image_rgb is None or image_seg is None:
-            return
-        image_rgb = cv2.cvtColor(image_rgb, cv2.COLOR_RGBA2RGB)
-        image_seg = cv2.cvtColor(image_seg, cv2.COLOR_BGRA2RGB)
-        img_name = os.path.basename(rgb_img_path)
-        height, width, _ = image_rgb.shape
-
-        labels_all = []
-        for index, label_info in LABEL_DATAFRAME.iterrows():
-            seg_color = label_info['color']
-            coco_id = label_info['coco_names_index']
-
-            mask = (image_seg == seg_color)
-            tmp_mask = (mask.sum(axis=2, dtype=np.uint8) == 3)
-            mono_img = np.array(tmp_mask * 255, dtype=np.uint8)
-
-            preview_img = image_rgb
-            # self.preview_img = self.image_seg
-            # cv2.imshow("seg", self.preview_img)
-            # cv2.imshow("mono", mono_img)
-            # cv2.waitKey()
-            # bordered_image = cv2.copyMakeBorder(mono_img, top=10, bottom=10, left=10, right=10, 
-            #                         borderType=cv2.BORDER_CONSTANT, value=[0,0,0])
-
-            # contours, _ = cv2.findContours(bordered_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-            contours, hierarchy = cv2.findContours(mono_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-            labels = []
-            for cnt in contours:
-                x, y, w, h = cv2.boundingRect(cnt)
-                if w * h < YoloConfig.rectangle_pixels_min:
-                    continue
-                # cv2.rectangle(self.preview_img, (x, y), (x + w, y + h), (0, 255, 0), 1)
-                # cv2.imshow("rect", self.preview_img)
-                # cv2.waitKey()
-                max_y, max_x, _ = image_rgb.shape
-                
-                # UNCOMMENT TO IGNORE BBOX THAT TOUCH THE EDGES
-                # if y + h >= max_y or x + w >= max_x:
-                #     continue
-
-                if coco_id == TL_LIGHT_LABEL["DEFAULT"]:
-                    coco_id = check_color(image_rgb[y:y + h, x:x + w, :])
-
-                # DEBUG START
-                # Draw label info to image
-                cv2.putText(preview_img, COCO_NAMES[coco_id], (x, y - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 1)
-                cv2.rectangle(image_rgb, (x, y), (x + w, y + h), (0, 255, 0), 1)
-                # DEBUG END
-
-                label_info = "{} {} {} {} {}".format(coco_id,
-                                                     float(x + (w / 2.0)) / width,
-                                                     float(y + (h / 2.0)) / height,
-                                                     float(w) / width,
-                                                     float(h) / height)
-                labels.append(label_info)
-                # cv2.imshow("result", self.preview_img)
-                # cv2.imshow("test", self.preview_img[y:y+h, x:x+w, :])
-                # cv2.waitKey()
-
-            # First, generate all bounding boxes with corresponding labels
-            # all_boxes = []
-            # for index, cnt in enumerate(contours):
-            #     x, y, w, h = cv2.boundingRect(cnt)
-            #     if w * h < YoloConfig.rectangle_pixels_min or hierarchy[0][index][3] != -1:
-            #         continue
-            #     max_y, max_x, _ = image_rgb.shape
-            #     all_boxes.append((x, y, w, h, index))
-
-            # # Filter out nested bounding boxes
-            # filtered_boxes = []
-            # for box1 in all_boxes:
-            #     x1, y1, w1, h1, index1 = box1
-            #     if not any((x1 > x2) and (y1 > y2) and (x1 + w1 < x2 + w2) and (y1 + h1 < y2 + h2) for x2, y2, w2, h2, index2 in all_boxes if index1 != index2):
-            #         filtered_boxes.append(box1)
-
-            # # Now filtered_boxes contains only the non-nested boxes.
-            # # We can process these further:
-            # for x, y, w, h, index in filtered_boxes:
-            #     if coco_id == TL_LIGHT_LABEL["DEFAULT"]:
-            #         coco_id = check_color(image_rgb[y:y + h, x:x + w, :])
-
-            #     # DEBUG START
-            #     # Draw label info to image
-            #     cv2.putText(preview_img, COCO_NAMES[coco_id], (x, y - 10),
-            #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 1)
-            #     cv2.rectangle(image_rgb, (x, y), (x + w, y + h), (0, 255, 0), 1)
-            #     # DEBUG END
-
-            #     label_info = "{} {} {} {} {}".format(coco_id,
-            #                                         float(x + (w / 2.0)) / width,
-            #                                         float(y + (h / 2.0)) / height,
-            #                                         float(w) / width,
-            #                                         float(h) / height)
-            #     labels.append(label_info)
-
-
-            if len(labels) > 0:
-                labels_all += labels
-
-        if len(labels_all) > 0:
-
-            # print("Label output path: {}".format(self.label_out_path))
-            write_image(output_dir, frame_id, image_rgb, frame["vehicle_name"], frame["split"])
-            write_label(output_dir, frame_id, labels_all, frame["vehicle_name"], frame["split"])
-        write_yaml(output_dir)
-        return
-
+    
 
 def main():
     argparser = argparse.ArgumentParser(description=__doc__)
     argparser.add_argument(
         '--record', '-r',
-        required=True,
+        default="1692557874.9057295",
         help='Rawdata Record ID. e.g. record_2022_0113_1337'
     )
     argparser.add_argument(
@@ -275,12 +162,12 @@ def main():
     )
     argparser.add_argument(
         '--rgb_camera', '-c',
-        default='image_2',
+        default='rgb',
         help='Camera name. e.g. image_2'
     )
     argparser.add_argument(
         '--semantic_camera', '-s',
-        default='image_2_instance',
+        default='instance',
         help='Camera name. e.g. image_2_semantic'
     )
 
@@ -294,13 +181,11 @@ def main():
         vehicle_name_list = [args.vehicle]
 
     yolo_label_tool = YoloLabelTool()
-    for vehicle_name in vehicle_name_list:
-        rawdata_df = gather_yolo_data(args.record,
-                                      vehicle_name,
-                                      args.rgb_camera,
-                                      args.semantic_camera)
-        print("Process {} - {}".format(record_name, vehicle_name))
-        yolo_label_tool.process(rawdata_df)
+    rawdata_df = gather_yolo_data(args.record,
+                                    args.rgb_camera,
+                                    args.semantic_camera)
+    # print("Process {} - {}".format(record_name, vehicle_name))
+    yolo_label_tool.process(rawdata_df)
 
 
 if __name__ == '__main__':
